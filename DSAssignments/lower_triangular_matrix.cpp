8cd847a2ef2217c458fa6a4cafa15f6355ac1ecc
Mr-Y-hub <yashpatidar251198@gmail.com>
2021-12-11 21:44:41 +0530

    Matrix Learning

---
+++ DSAssignments/lower_triangular_matrix.cpp
@@ -0,0 +1,84 @@
+// Lower triangular matix : M[i,j] = 0 if i<j, else M[i,j] != 0 if i>=j
+//
+// Number of non-zero elements = n(n+1)/2
+//
+// Number of zero elements = n^2 - Number of non-zero elements = n(n-1)/2
+//
+// size of array required -->  n(n+1)/2
+//
+// access element --->   M[i,j] = array[(i*(i-1)/2) + (j-1)]  --> row major
+// 
+// access element --->   M[i,j] = array[( (n*(j-1)) - (j-2)(j-1)/2 )  +  (i-j) ]  --> column major
+
+
+#include <iostream>
+using namespace std;
+
+class Lower_Triangular_Matrix
+{
+private:
+    int matrix_size;
+    int arr_size;
+    int *arr;
+
+public:
+    Lower_Triangular_Matrix(int size)
+    {
+        this->matrix_size = size;
+        this->arr_size = size * (size + 1) / 2;
+        this->arr = new int[this->arr_size];
+    }
+
+    ~Lower_Triangular_Matrix()
+    {
+        delete[] arr;
+    }
+
+    void set(int i, int j, int num)
+    {
+        if (i >= j)
+            this->arr[(i * (i - 1) / 2) + (j - 1)] = num;
+    }
+
+    int get(int i, int j)
+    {
+        if (i >= j)
+            return this->arr[(i * (i - 1) / 2) + (j - 1)];
+        else
+            return 0;
+    }
+
+    void Display()
+    {
+        for (int i = 1; i <= this->matrix_size; i++)
+        {
+            for (int j = 1; j <= this->matrix_size; j++)
+            {
+                cout << this->get(i, j) << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Lower_Triangular_Matrix obj(5);
+    cout<<"Enter Number to show in Lower triangular Matrix : ";
+    int num;
+    cin>>num;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (i >= j)
+            {   
+                obj.set(i, j, num);
+            }
+        }
+    }
+    cout<<"Lower triangular matrix "<<endl;
+    obj.Display();
+
+    return 0;
+}
